fef48ffccf5cf1f387d160bf1694fa87bbfda42c
judel fintch <judfintch@gmail.com>
2023-06-15 11:10:32 +0200

    feat :rules validate done

--- app/Http/Livewire/FormInscription/FormInscription.php
+++ app/Http/Livewire/FormInscription/FormInscription.php
@@ -3,13 +3,17 @@
 namespace App\Http\Livewire\FormInscription;
 
 use Livewire\Component;
+use app\Models\Students as Students;
+use app\Models\Parents as Parents;
+use app\Models\Health as Health;
+use app\Models\Sollicitation as Sollicitation;
+
 
 class FormInscription extends Component
 {
 
 //st =student
-    public $st_name,$st_sexe,$st_birthdate,$st_birthdayplace, $st_spoken,$st_phone,$st_province,$st_district,$st_pays,$st_addres;
-
+    public $st_name,$st_sexe,$st_birthdate,$st_birthdayplace, $st_spoken,$st_phone,$st_province,$st_district,$st_pays,$st_addres, $options_values;
     public $first_parent_name,$second_parent_name,$first_contact,$second_contact;
     public $section,$options,$classe,$health,$section_values= array(), $classe_values=array();
 
@@ -22,57 +26,94 @@ public function mount(){
 
 public function sections(){
 //a mettre dans une base de donnee
-    $mat_options =array('Creche','Premat','Maternelle');
+    $creche_options =array('Creche');
+    $premat_options =array('Pre-mat');
+    $mat_options =array('Maternelle');
     $prim_options =array('Primaire');
     $sec_eb_options =array('Enseignement de base');
     $sec_gen_options =array('Scientifique','Litteraire','Pedagogie General');
     $sec_tech_options =array('Construction et Architecture','Coupe et Couture','Electricite','Commercial et Gestion','Hotellerie et Restaurant','Electronique');
   ///a dynamyse avec une base de donnee
     $classe_creche = array('Creche');
+    $classe_premat = array('Creche');
     $classe_mat = array('1','2','3');
     $classe_eb = array('7','8');
-    $classe_elem= array('3','4','5','6','7','8');
+    $classe_elem= array('3','4','5','6');
     $classe_prim= array('1','2','3','4','5','6');
 
 
     if($this->section == 1){
+    $this->section_values = $creche_options;
+    $this->classe_values =$classe_creche;
+    }
+
+    if($this->section == 2){
+    $this->section_values = $premat_options;
+    $this->classe_values =$classe_premat;
+    }
+
+    if($this->section == 3){
     $this->section_values = $mat_options;
     $this->classe_values =$classe_mat;
     }
 
-    if($this->section == 2){
-    $this->section_values = $prim_options;
-    }
-
-    if($this->section == 3){
-    $this->section_values = $sec_eb_options;
-    }
-
     if($this->section == 4){
-    $this->section_values = $sec_gen_options;
+    $this->section_values = $prim_options;
+    $this->classe_values =$classe_prim;
     }
 
     if($this->section == 5){
-    $this->section_values = $sec_tech_options;  
+    $this->section_values = $sec_eb_options;
+    $this->classe_values =$classe_eb;
+    }
+
+    if($this->section == 6){
+    $this->section_values = $sec_gen_options;
+    $this->classe_values =$classe_elem;
+    }
+
+    if($this->section == 7){
+    $this->section_values = $sec_tech_options; 
+    $this->classe_values =$classe_elem; 
 }
 }
+
+
+protected $rules =
+        [
+            'st_name' => 'required',
+            'st_birthdate' => 'required',
+            'st_birthdayplace' =>'required',
+            'st_spoken' =>'required',
+            'st_province'  =>'required',
+            'st_district'  =>'required',
+            'st_pays'  =>'required',
+            'st_addres'  =>'required',
+            'first_parent_name'  =>'required',
+            'second_parent_name'  =>'required',
+            'first_contact'  =>'required',
+            'section'  =>'required',
+            'st_phone'  =>'required',
+            'options_values'  =>'required',
+            'classe'  =>'required',
+           
+            
+            
+        ] ;
 public function render()
 {
-
 $section = $this->section;
 return view('livewire.form-inscription.form-inscription', compact('section'));
 }
-
 
 public function classes(){
 
 }
 
 
-
-
-
 public function store(){
+
+   // $this->validate();
 
 }
 
--- resources/views/livewire/form-inscription/form-inscription.blade.php
+++ resources/views/livewire/form-inscription/form-inscription.blade.php
@@ -7,7 +7,7 @@
         </h1>
         <p class="text-xl font-bold text-gray-900">Annee scolaire : 2023-2024</p>
       </div>
-      <form action="" class="space-y-12 pb-12">
+     
         <div class="bg-white rounded-3xl p-6 sm:p-10 lg:p-12 border border-gray-200">
           <h2 class="text-xl font-bold text-gray-900 mb-5">
             IDENTITE DE L'ELEVE
@@ -17,52 +17,60 @@
               <div class="space-y-2">
                 <label for="name" class="text-gray-600">Nom complet</label>
                 <input wire:model='st_name' type="text" name="name" id="name" autocomplete="name" placeholder=" Ex Kayembe Mulombo Enock"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_name') is-invalid @enderror"/>
+
+                  @error('st_name') <span class="error">{{ $message }}</span> @enderror
               </div>
               <div class="space-y-2">
                 <label for="email" class="text-gray-600">Genre</label>
                 <select wire:model='st_sexe' name="" id=""
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400">
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_sexe') is-invalid @enderror">
                   <option value="masculin">Masculin</option>
                   <option value="feminin">Feminin</option>
                 </select>
+                @error('st_sexe') <span class="error">{{ $message }}</span> @enderror
               </div>
 
               <div class="space-y-2">
                 <label for="name" class="text-gray-600">Date de naissance</label>
                 <input wire:model='st_birthdate' type="date" name="name" id="name" autocomplete="name"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_birthdate') is-invalid @enderror" />
               </div>
               <div class="space-y-2">
                 <label for="email" class="text-gray-600">Lieu de naissance</label>
                 <input wire:model='st_birthdayplace' type="text" name="name" id="name" autocomplete="name" placeholder="Kaniama"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_birthdayplace') is-invalid @enderror" />
+                  @error('st_birthdayplace') <span class="error">{{ $message }}</span> @enderror
               </div>
 
               <div class="space-y-2">
                 <label for="email" class="text-gray-600">Langue(s) parlée(s) </label>
                 <input wire:model='st_spoken' type="text" name="name" id="name" autocomplete="name" placeholder="Swahili, Francais, Anglais"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_spoken') is-invalid @enderror" />
+                  @error('st_spoken') <span class="error">{{ $message }}</span> @enderror
               </div>
 
               <div class="space-y-2">
                 <label for="email" class="text-gray-600">Telephone </label>
-                <input wire:model='st_st_phone' type="tel" name="name" id="name" autocomplete="name" placeholder="+243 97 24 44 966"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                <input wire:model='st_phone' type="tel" name="name" id="name" autocomplete="name" placeholder="+243 97 24 44 966"
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  @error('st_phone') is-invalid @enderror" />
+                  @error('st_phone') <span class="error">{{ $message }}</span> @enderror
               </div>
 
               <div class="space-y-2">
                 <label for="province-origine" class="text-gray-600">Province d'origine * </label>
                 <input wire:model='st_province' type="text" name="province-origine" id="province-origine" autocomplete="province-origine"
                   placeholder="Haut lomami"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('st_province') is-invalid @enderror" />
+                  @error('st_province') <span class="error">{{ $message }}</span> @enderror
               </div>
 
               <div class="space-y-2">
                 <label for="district-origine" class="text-gray-600">District d'origine * </label>
                 <input wire:model='st_district' type="text" name="district-origine" id="district-origine" autocomplete="district-origine"
                   placeholder="Haut lomami"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('st_district') is-invalid @enderror" />
+                  @error('st_district') <span class="error">{{ $message }}</span> @enderror
               </div>
 
 
@@ -70,13 +78,15 @@
                 <label for="pays-origine" class="text-gray-600">Pays d'origine :* </label>
                 <input wire:model='st_pays' type="text" name="pays-origine" id="pays-origine" autocomplete="pays-origine"
                   placeholder="Haut lomami"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('st_pays') is-invalid @enderror" />
+                  @error('st_pays') <span class="error">{{ $message }}</span> @enderror
               </div>
               <div class="space-y-2 sm:col-span-2">
                 <label for="adresse-p" class="text-gray-600">Adresse physique de l'éléve </label>
                 <input  wire:model='st_addres' type="text" name="adresse-p" id="adresse-p" autocomplete="adresse-p"
                   placeholder="Q/Kasapa, Av circulaire, No 293"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('st_addres') is-invalid @enderror" />
+                  @error('st_addres') <span class="error">{{ $message }}</span> @enderror
               </div>
             </div>
           </div>
@@ -92,23 +102,27 @@
                 <label for="name-father" class="text-gray-600">Noms du père:*</label>
                 <input wire:model='first_parent_name' type="text" name="name-father" id="name-father" autocomplete="name"
                   placeholder="Kayembe Mulombo Enock"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('first_parent_name') is-invalid @enderror" />
+                  @error('first_parent_name') <span class="error">{{ $message }}</span> @enderror
               </div>
               <div class="space-y-2">
                 <label for="name-mother" class="text-gray-600">Noms de la mère:*</label>
                 <input wire:model='second_parent_name' type="text" name="name-mother" id="name-mother" autocomplete="name"
                   placeholder="Kayembe Mulombo Enock"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('second_parent_name') is-invalid @enderror" />
+                  @error('second_parent_name') <span class="error">{{ $message }}</span> @enderror
               </div>
               <div class="space-y-2">
                 <label for="contact1" class="text-gray-600">Contact Parent 1</label>
                 <input wire:model='first_contact' type="tel" name="contact1" id="contact1" autocomplete="name" placeholder="Kayembe Mulombo Enock"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 @error('first_contact') is-invalid @enderror" />
+                  @error('first_contact') <span class="error">{{ $message }}</span> @enderror
               </div>
               <div class="space-y-2">
                 <label for="contact2" class="text-gray-600">Contact Parent 2</label>
                 <input wire:model='second_contact' type="tel" name="contact2" id="contact2" autocomplete="name" placeholder="Kayembe Mulombo Enock"
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400" />
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400  " />
+                 
               </div>
             </div>
           </div>
@@ -123,19 +137,22 @@
               <div class="space-y-2">
                 <label for="name-father" class="text-gray-600">Sections :*</label>
                 <select wire:click='sections()' wire:model="section" name="" id=""
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400">
-                  <option value="1">Maternelle</option>
-                  <option value="2">Primaire</option>
-                  <option value="3">EB</option>
-                  <option value="4">Secondaire General</option>
-                  <option value="5">Secondaire Technique</option>
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 ">
+                  <option value="1">Creche</option>
+                  <option value="2">Pre-mat</option>
+                  <option value="3">Maternelle</option>
+                  <option value="4">Primaire</option>
+                  <option value="5">EB</option>
+                  <option value="6">Secondaire General</option>
+                  <option value="7">Secondaire Technique</option>
                   
                 </select>
+                
               </div>
               <div class="space-y-2">
                 <label for="name-mother" class="text-gray-600">Option :*</label>
-                <select name="" id=""
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400">
+                <select wire:model.defer='options_values' name="" id=""
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 ">
                     @foreach($section_values as $option)
                         <option>{{$option}}</option>
                     @endforeach
@@ -146,13 +163,14 @@
               <div class="space-y-2">
                 <label for="contact1" class="text-gray-600">Classe :*</label>
                 <select wire:model="classe" name="" id=""
-                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400">
+                  class="outline-none block w-full rounded-xl border-2 border-gray-100 bg-gray-100 px-4 py-2.5 text-sm md:text-base text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:border-blue-400 ">
                   @foreach($classe_values as $classe)
                         <option>{{$classe}}</option>
                     @endforeach
                  
                 </select>
               </div>
+             
             </div>
           </div>
         </div>
@@ -174,12 +192,12 @@
           </div>
         </div>
         <div class="flex justify-end">
-          <button
+          <button type="button" wire:click.prevent="store()"
             class="px-4 py-2.5 rounded-lg outline-none bg-blue-600 text-white transition hover:bg-blue-700 focus:bg-blue-500">
             Enregistrer
           </button>
         </div>
-      </form>
+     
     </div>
   </section>
 </div>
